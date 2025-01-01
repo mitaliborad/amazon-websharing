491d8817825ae4ef682479e47caadfd1687aec23
mitaliborad <mitaliia109@gmail.com>
2025-01-01 11:14:39 +0530

    update locator

--- Amazon websharing.py
+++ Amazon websharing.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.keys import Keys # in output press enter , tab, b
 from selenium.webdriver.common.by import By # for how to find element (by id, class, name)
 from selenium.webdriver.chrome.options import Options #for reduce detection
 from bs4 import BeautifulSoup #use for parsing HTML & XML
+from selenium.webdriver.support.ui import WebDriverWait # for waiting while all product elements appear on the page
+from selenium.webdriver.support import expected_conditions as EC
 import time # for pause time
 import pandas as pd #for data collect in csv
 import os # import os module in python
@@ -40,6 +42,35 @@ for i in range(1,10):
             file +=1
         print(elem.text)
 time.sleep(2)
+for i in range(0,11):
+    url = f"https://www.amazon.in/s?k={query}&page={i}"
+    driver.get(url)
+
+    try:
+        # Wait for products to load 5 untill find expected condition EC
+        #presence_of_all_elements_located = expected condition used in conjunction with WebDriverWait
+        #.s-main-slot = container holding all the products.
+        #.s-result-item represents each individual product within that container.
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".s-main-slot .s-result-item"))
+        )#web driver waits 10 seconds untill the our search result appear
+        elems = driver.find_elements(By.CSS_SELECTOR, ".s-main-slot .s-result-item")
+        # all the search result items  that are located inside the main search results container 
+        print(f"Page {i}: {len(elems)} items found")
+
+        for elem in elems:
+            try:
+                d = elem.get_attribute("outerHTML") # Retrieves HTML structure for that product element.
+                with open(f"data/{query}_{file}.html", "w", encoding="utf-8") as f:
+                    f.write(d)
+                print(f"Saved item {file}")
+                file += 1
+            except Exception as e:
+                print(f"Error saving item {file}: {e}")
+    except Exception as e:
+        print(e)
+
+time.sleep(4)
 driver.close()
 
 
@@ -58,17 +89,30 @@ for file in os.listdir("data") : # file in data folder
         # find h2 for the title
         t = l.find("h2") 
         title = t.get_text()
+ #find a tag with class which has link and title
+        # select-one = the first matching element.
+        #.a-link-normal = class
+        l = soup.select_one("a.a-link-normal.s-line-clamp-2.s-link-style.a-text-normal[href]") 
+ 
+        #using select-one CSS SELECTOR find title
+        title = l.select_one("h2").get_text() 
         print("Title:", title)
         
         
         # find link 
         link = "https://amazon.in/" + l['href']
+
+        #link
+        link = "https://amazon.in" + l["href"]
         print("Link:", link)
             
         
         # find price in a-price-whole class in new attribute
         p = soup.find("span",attrs={"class" : 'a-price-whole'}) 
         price = (p.get_text()) 
+        # using select-one CSS SELECTOR find price
+        p = soup.select_one("span.a-price-whole")
+        price = p.get_text() 
         print("Price:", price)
         
         
