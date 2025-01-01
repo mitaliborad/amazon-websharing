d89d2a032f5d6c70556f611925ae1395126e7e31
mitaliborad <mitaliia109@gmail.com>
2025-01-01 11:16:15 +0530

    update locators

--- Amazon websharing.py
+++ Amazon websharing.py
@@ -1,6 +1,5 @@
 #import statements
 from selenium import webdriver # open and controls web browser
-from selenium.webdriver.common.keys import Keys # in output press enter , tab, backspace
 from selenium.webdriver.common.by import By # for how to find element (by id, class, name)
 from selenium.webdriver.chrome.options import Options #for reduce detection
 from bs4 import BeautifulSoup #use for parsing HTML & XML
@@ -26,22 +25,6 @@ query = "laptop"
 file = 0
 
 #Loops through the first 10  pages of search results.
-for i in range(1,10):
-    driver.get(f"https://www.amazon.in/s?k={query}&page=3&crid=2MW5PS1STZ96S&qid=1735275154&sprefix=%2Caps%2C265&ref=sr_pg_{i}")
-
-    #Finds all elements matching the class name puis-card-container
-    elems = driver.find_elements(By.CLASS_NAME, "puis-card-container")
-    #Prints the number of items found on the page.
-    print(f"{len(elems)} items found")
-    print(elems)
-    for elem in elems:
-        #Saves the HTML content of each element to separate .html files in a folder named data.
-        d = elem.get_attribute("outerHTML")
-        with open(f"data/{query}_{file}.html","w", encoding="utf-8") as f:
-            f.write(d)
-            file +=1
-        print(elem.text)
-time.sleep(2)
 for i in range(0,11):
     url = f"https://www.amazon.in/s?k={query}&page={i}"
     driver.get(url)
@@ -83,12 +66,6 @@ for file in os.listdir("data") : # file in data folder
             html_doc = f.read()
         soup = BeautifulSoup(html_doc, 'html.parser') 
 
-        #find a tag with class which has link and title
-        l = soup.find("a",class_="a-link-normal s-line-clamp-2 s-link-style a-text-normal", href=True)
-
-        # find h2 for the title
-        t = l.find("h2") 
-        title = t.get_text()
  #find a tag with class which has link and title
         # select-one = the first matching element.
         #.a-link-normal = class
@@ -97,19 +74,12 @@ for file in os.listdir("data") : # file in data folder
         #using select-one CSS SELECTOR find title
         title = l.select_one("h2").get_text() 
         print("Title:", title)
-        
-        
-        # find link 
-        link = "https://amazon.in/" + l['href']
 
         #link
         link = "https://amazon.in" + l["href"]
         print("Link:", link)
             
         
-        # find price in a-price-whole class in new attribute
-        p = soup.find("span",attrs={"class" : 'a-price-whole'}) 
-        price = (p.get_text()) 
         # using select-one CSS SELECTOR find price
         p = soup.select_one("span.a-price-whole")
         price = p.get_text() 
